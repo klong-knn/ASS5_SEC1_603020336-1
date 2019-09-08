222fcb10bd3d9aeae9fe4d58c6d304b5fc841814
klong-knn <54529534+klong-knn@users.noreply.github.com>
2019-09-08 22:14:50 +0700

    ASS5_SEC1_603020336-1

---
+++ app/src/main/java/com/example/ass5_sec1_603020336_1/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.ass5_sec1_603020336_1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.myweb.ass5.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
